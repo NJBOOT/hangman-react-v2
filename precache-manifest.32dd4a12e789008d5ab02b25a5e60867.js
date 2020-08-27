e43e44673ec0ff26cb26bad65f9b434aed8984ae
Nico Barbarese <nicobarbarese@gmail.com>
2020-08-27 01:10:54 -0400

    Updates

---
+++ precache-manifest.32dd4a12e789008d5ab02b25a5e60867.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1730a18c2c4911a1c7e4ab22be7de9",
+    "url": "/hangman-react-v2/index.html"
+  },
+  {
+    "revision": "c64614ee0e207360ce6d",
+    "url": "/hangman-react-v2/static/css/main.2222e3af.chunk.css"
+  },
+  {
+    "revision": "a40c8e1b777b6e82d452",
+    "url": "/hangman-react-v2/static/js/2.ade7de5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman-react-v2/static/js/2.ade7de5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c64614ee0e207360ce6d",
+    "url": "/hangman-react-v2/static/js/main.df9750c3.chunk.js"
+  },
+  {
+    "revision": "61c9f6b1526d4464ea74",
+    "url": "/hangman-react-v2/static/js/runtime-main.5617d9d5.js"
+  }
+]);
